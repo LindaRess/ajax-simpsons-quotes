82fb9162f3ea1cc05b25917f008b12bb6c589e32
“Linda <linda.resseguier@gmail.com>
2020-10-26 11:50:05 +0100

    simpsons quotes added

---
+++ app.js
@@ -0,0 +1,19 @@
+function fetchSimpsonJSON() {
+const url = "https://simpsons-quotes-api.herokuapp.com/quotes";
+    axios.get(url)
+        .then(function(response) {
+            return response.data[0]; // response.data instead of response.json() with fetch
+        })
+        .then(function(simpson) {
+            console.log('data decoded from JSON:', simpson);
+  
+        // Build a block of HTML
+        const simpsonHtml = `
+          <p><strong>${simpson.character}</strong></p>
+          <img src="${simpson.image}" />
+        `;
+        document.querySelector('#simpson-quote').innerHTML = simpsonHtml;
+});
+}
+  
+fetchSimpsonJSON();
